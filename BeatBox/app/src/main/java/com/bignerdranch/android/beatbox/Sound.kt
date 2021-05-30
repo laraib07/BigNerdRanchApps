943b7e4527938619de659af3007997f1efd96868
laraib <laraib@pop-os.localdomain>
2021-05-30 17:26:15 +0530

    added beatbox

---
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/Sound.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.beatbox
+
+private const val WAV = ".wav"
+
+class Sound(val assetPath: String, var soundId: Int? = null) {
+    /**
+     * class to manage sound
+     */
+
+    val name = assetPath.split("/").last().removeSuffix(WAV)
+}
